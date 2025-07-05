7f638062978465850e1d71d8f797f76db6398857
warriortechsol <warriortech-solutions@outlook.com>
2025-07-04 20:19:46 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PredictionPanel from './components/PredictionPanel'
+
+export default function App() {
+  return (
+    <div>
+      <h1>🚀 CryptoOracle Pulse</h1>
+      <PredictionPanel />
+    </div>
+  )
+}
