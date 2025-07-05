96e7fc6891c701db87c04a84efccc6ad5b85f7da
Aaron Dennis <aaron.dennis@Aarons-MacBook-Air.local>
2025-07-05 03:17:54 -0400

    Restore App.jsx and main.jsx for CryptoOracle render

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cryptooracle/', // adjust based on GitHub repo name
+  base: '/CryptoOracle-Frontend/'
 })
+
+
