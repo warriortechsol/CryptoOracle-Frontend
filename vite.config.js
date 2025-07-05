25ce439fea49b3612bd08187559ba7365e858bbb
warriortechsol <warriortech-solutions@outlook.com>
2025-07-04 21:06:40 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cryptooracle/', // Make sure this matches your GitHub repo name
+  base: '/cryptooracle/' // Only needed for GitHub Pages
 })
